608d5da240068a1c4982ca9617333bfb01c84bc5
IagoCoelho <iagocoelhos@gmail.com>
2023-10-02 21:16:40 -0300

    done

---
+++ src/main/java/br/com/fiap/epicdask/EpicdaskApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.epicdask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicdaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicdaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/epicdask/EpicdaskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.epicdask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicdaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
